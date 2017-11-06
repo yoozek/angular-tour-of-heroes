f6ac22d7823c3d5959019ee80576323ef8fab892
Łukasz Jóźwik <jozwiklukasz@outlook.com>
2017-11-07 00:13:57 +0100

    Introduce selectedHero, prevent null referebce issue

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -10,12 +10,12 @@ import {Hero} from '../hero'
   encapsulation: ViewEncapsulation.None
 })
 export class HeroesComponent implements OnInit {
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
-
+  selectedHero: Hero;
   heroes = HEROES;
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 
   constructor() { }
 
