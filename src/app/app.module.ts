07e8824a22a13e6f7116e6f407512a06a02cbbdd
Łukasz Jóźwik <jozwiklukasz@outlook.com>
2017-11-07 22:17:48 +0100

    Introduce rxJS observable

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms'
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
@@ -16,6 +17,7 @@ import { HeroService } from './hero.service';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     FormsModule
   ],
   providers: [HeroService],
--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
-
+import { Observable } from 'rxjs/Rx';
+import { of } from 'rxjs/observable/of';
 import { Hero } from './hero';
 import { HEROES } from './heroes/mock-heroes';
 
@@ -8,7 +9,7 @@ export class HeroService {
 
   constructor() { }
 
-  getHeroes() : Hero[] {
-    return HEROES;
+  getHeroes() : Observable<Hero[]>   {
+    return of(HEROES);
   }
 }
